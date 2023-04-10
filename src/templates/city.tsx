92b87ff3fcb85aeae92900c0cfd5271ac89deca2
Yextds <yext@dotsquares.com>
2023-04-10 17:25:35 +0530

    updated city page

--- src/templates/city.tsx
+++ src/templates/city.tsx
@@ -105,119 +105,119 @@ const City: Template<TemplateRenderProps> = ({
   const templateData = { document: document, __meta: __meta };
   let address;
   let childrenDivs = <></>;
-  if(typeof document.dm_directoryChildren != 'undefined' && document.dm_directoryChildren.length > 0){
+  if (typeof document.dm_directoryChildren != 'undefined' && document.dm_directoryChildren.length > 0) {
     childrenDivs = dm_directoryChildren.map((entity: any) => {
-    // let url = "";
-    // const name: any = entity.name;
-    // const string: any = name.toString();
-    // const result: any = string.replaceAll(" ", "-");
-    // const finalslug:any=constant.slugify(entity.slug);
-    // if (!entity.slug) {
-    //   url = `/${entity.id}-${result}`;
-    // } else {
-    //   url = `/${finalslug}`;
-    // }
+      let url = entity.name;
+      // const name: any = entity.name;
+      // const string: any = name.toString();
+      // const result: any = string.replaceAll(" ", "-");
+      // const finalslug:any=constant.slugify(entity.slug);
+      // if (!entity.slug) {
+      //   url = `/${entity.id}-${result}`;
+      // } else {
+      //   url = `/${finalslug}`;
+      // }
 
-    return (
-      <div className="nearby-card">
-        <div className="location-name-miles icon-row">
-          <h2>
-            {/* <Link
-              className="inline-block notHighlight"
+      return (
+        <div className="nearby-card">
+          <div className="location-name-miles icon-row">
+            <h2>
+              <Link
+                className="inline-block notHighlight"
+                href={url}
+                data-ya-track={`viewstore-${entity.name}`}
+                eventName={`viewstore-${entity.name}`}
+                rel="noopener noreferrer"
+              >
+                {entity.name}
+              </Link>
+            </h2>
+          </div>
+          <div className="icon-row">
+            <Address address={entity.address} />
+          </div>
+
+          <div className="icon-row">
+            <div className="content-col open-now-string">
+              {typeof entity.hours?.reopenDate != "undefined" ? (
+                <h6>{StaticData.tempClosed}</h6>
+              ) : (
+                <OpenClose timezone={entity.timezone} hours={entity.hours} />
+              )}
+            </div>
+          </div>
+
+
+          <div className="button-bx">
+            <Link
+              className="btn"
               href={url}
               data-ya-track={`viewstore-${entity.name}`}
               eventName={`viewstore-${entity.name}`}
               rel="noopener noreferrer"
             >
-              {entity.name}
-            </Link> */}
-          </h2>
-        </div>
-        {/* <div className="icon-row">
-          <Address address={entity.address} />
-        </div> */}
-
-        {/* <div className="icon-row">
-          <div className="content-col open-now-string">
-            {typeof entity.hours?.reopenDate != "undefined" ? (
-              <h6>{StaticData.tempClosed}</h6>
-            ) : (
-              <OpenClose timezone={entity.timezone} hours={entity.hours} />
-            )}
+              {StaticData.StoreDetailbtn}
+            </Link>
+            <GetDirection
+              buttonText={StaticData.getDirection}
+              address={entity.address}
+              googlePlaceId={entity.googlePlaceId}
+              latitude={entity.yextDisplayCoordinate.latitude}
+              longitude={entity.yextDisplayCoordinate.longitude}
+            />
           </div>
-        </div> */}
+        </div>
+      );
+    });
+  } else {
+    childrenDivs = <div className="content-list !pt-4">
+      <div className="container">
+        <div className="sec-title text-center">
+          <h1 className="text-xl font-second-main-font !text-gray" style={{ textAlign: "center" }}>
+            {StaticData.PagenotFound}
+          </h1>
+          <p className="text-xl">
+            We're really sorry, we can't find any open store in {document.name}.
+          </p>
+          <p className="text-lg mt-3 font-second-main-font">
+            {StaticData.Youcouldtry}
+          </p>
 
-
-        {/* <div className="button-bx">
-          <Link
-            className="btn"
-            href={url}
-            data-ya-track={`viewstore-${entity.name}`}
-            eventName={`viewstore-${entity.name}`}
-            rel="noopener noreferrer"
-          >
-            {StaticData.StoreDetailbtn}
-          </Link>
-          <GetDirection
-            buttonText={StaticData.getDirection}
-            address={entity.address}
-            googlePlaceId={entity.googlePlaceId}
-            latitude={entity.yextDisplayCoordinate.latitude}
-            longitude={entity.yextDisplayCoordinate.longitude}
-          />
-        </div> */}
+          <div className="button-bx  !flex-col max-w-[45rem] !mx-auto !mt-3">
+            <a
+              className=" max-w-[20.5rem] mx-auto border-b border-dotted hover:text-red hover:border-red "
+              href="/gb"
+            >
+              {" View all available location in United Kingdom >"}
+            </a>
+            <a
+              className="max-w-[20.5rem] mx-auto border-b border-dotted hover:text-red hover:border-red"
+              href="/"
+            >
+              {" "}
+              {StaticData.homePage}
+            </a>
+          </div>
+        </div>
+        <div className="department-sec">
+          <h3 className="text-center first-letter:text-xl font-second-main-font !text-gray">
+            {_site.c_shopByDepartmentHeading}
+          </h3>
+          <ul className="sec-title text-center ">
+            {_site.c_departmentsCTA.map((res: any) => {
+              if (res.link) {
+                return (
+                  <>
+                    <li className="mx-6"> <a href={res.link}>{res.label}</a></li>
+                  </>
+                );
+              }
+            })}
+          </ul>
+        </div>
       </div>
-    );
-  });
-}else{
-//   childrenDivs =  <div className="content-list !pt-4">
-//   <div className="container">
-//     <div className="sec-title text-center">
-//           <h1 className="text-xl font-second-main-font !text-gray" style={{ textAlign: "center" }}>
-//                     {StaticData.PagenotFound}
-//                   </h1>
-//           <p className="text-xl">
-//           We're really sorry, we can't find any open store in {document.name}. 
-//           </p>
-//           <p className="text-lg mt-3 font-second-main-font">
-//                     { StaticData.Youcouldtry}
-//           </p>
-          
-//       <div className="button-bx  !flex-col max-w-[45rem] !mx-auto !mt-3">
-//         <a
-//           className=" max-w-[20.5rem] mx-auto border-b border-dotted hover:text-red hover:border-red "
-//           href="/gb"
-//         >
-//       {" View all available location in United Kingdom >"}
-//         </a>
-//         <a
-//           className="max-w-[20.5rem] mx-auto border-b border-dotted hover:text-red hover:border-red"
-//           href="/"
-//         >
-//           {" "}
-//           {StaticData.homePage}
-//         </a>
-//       </div>
-//     </div>
-//     <div className="department-sec">
-//               <h3 className="text-center first-letter:text-xl font-second-main-font !text-gray">
-//                 {_site.c_shopByDepartmentHeading}
-//               </h3>
-//               <ul className="sec-title text-center ">
-//                 {_site.c_departmentsCTA.map((res: any) => {
-//                   if (res.link) {
-//                     return (
-//                       <>
-//                        <li className="mx-6"> <a href={res.link}>{res.label}</a></li>
-//                       </>
-//                     );
-//                   }
-//                 })}
-//               </ul>
-//             </div>
-//   </div>
-// </div>
-}
+    </div>
+  }
 
   // let url: any = "";
   // document.dm_directoryParents && document.dm_directoryParents.map((i: any) => {
@@ -255,9 +255,9 @@ const City: Template<TemplateRenderProps> = ({
                     {StaticData.AllStores} {name}
                   </h1>
                 </div>
-                {/* <div className={`flex flex-wrap justify-center items-start -mx-2.5 lg:-mx-[.9375rem] ${!dm_directoryChildren && "!block"}`}>
+                <div className={`flex flex-wrap justify-center items-start -mx-2.5 lg:-mx-[.9375rem] ${!dm_directoryChildren && "!block"}`}>
                   <div className={`nearby-sec-inner ${(dm_directoryChildren && dm_directoryChildren.length == 1) && "w-full"} ${!dm_directoryChildren && "!block"}`}>{childrenDivs}</div>
-                </div> */}
+                </div>
               </div>
             </div>
           </PageLayout>
